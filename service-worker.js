c1bde2a97ddf973d428f4ba3f245236d46e75256
dheeram <dheeram@users.noreply.github.com>
2020-04-27 13:19:50 +0000

    Deploying to gh-pages from  @ 200407144347087797b570f0cc1eded56c9a204e 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.059d79fef27fe2039268f9be908b0c88.js"
+  "/precache-manifest.2bfa2c9f6d3f001f5c67fc492a581f50.js"
 );
 
 self.addEventListener('message', (event) => {
